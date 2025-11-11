7b279d1a9a4ededa1ed467add538522e06c906fe
Maria <mariatimina2005@gmail.com>
2025-11-11 16:43:42 +0300

    создание пользователей только для менеджера

--- api-gateway/controllers/userController.js
+++ api-gateway/controllers/userController.js
@@ -1,5 +1,6 @@
 const { USERS_SERVICE_URL } = require('../config/services');
 const usersCircuit = require('../circuits/usersCircuit');
+const bcrypt = require('bcryptjs');
 
 exports.getAllUsers = async (req, res) => {
   try {
@@ -27,10 +28,27 @@ exports.getUser = async (req, res) => {
 
 exports.createUser = async (req, res) => {
   try {
+    const { name, password, email, roles } = req.body;
+
+    if (!name || !password || !email || !roles) {
+      return res.status(400).json({ success: false, error: 'Не все поля заполнены' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await usersCircuit.fire(`${USERS_SERVICE_URL}/users`, {
       method: 'POST',
-      data: req.body
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        roles
+      }
     });
+    if (!user.success) {
+      return res.status(404).json({
+          success: false,
+          error: user.error || 'Ошибка при создании пользователя'
+        });
+      }
     res.status(201).json(user);
   } catch {
     res.status(500).json({ error: 'Internal server error' });
