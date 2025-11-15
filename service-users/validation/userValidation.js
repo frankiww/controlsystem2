c6342d9297c38d909fd400ef0ada067ab1ba4122
Maria <mariatimina2005@gmail.com>
2025-11-15 15:00:29 +0300

    изменила валидацию пользователя

--- service-users/validation/userValidation.js
+++ service-users/validation/userValidation.js
@@ -1,11 +1,11 @@
 const { z } = require('zod');
 
 exports.userValidation = z.object({
-    email: z.string().email(),
-    password: z.string(),
-    name: z.string().min(1),
+    email: z.string().email().optional(),
+    password: z.string().optional(),
+    name: z.string().min(1).optional(),
     roles: z.array(z.number().int()).nonempty().refine(
         (roles) => roles.every(r => [1,2,3].includes(r))
-    )
+    ).optional()
 });
 
